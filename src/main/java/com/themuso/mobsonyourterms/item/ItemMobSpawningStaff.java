0302308410ceb04d87a49cb4f1789fe3b9bb4e03
TheMuso <9444357+TheMuso@users.noreply.github.com>
2015-03-01 10:46:43 +1100

    Add tooltips for the staff fragment and spawning staff

--- src/main/java/com/themuso/mobsonyourterms/item/ItemMobSpawningStaff.java
+++ src/main/java/com/themuso/mobsonyourterms/item/ItemMobSpawningStaff.java
@@ -1,6 +1,15 @@
 package com.themuso.mobsonyourterms.item;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
 import com.themuso.mobsonyourterms.reference.Names;
+import com.themuso.mobsonyourterms.utility.ItemNBTHelper;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemMobSpawningStaff extends ItemMOYT
 {
@@ -9,5 +18,14 @@ public class ItemMobSpawningStaff extends ItemMOYT
 		super();
 		this.setUnlocalizedName(Names.Items.MOB_SPAWNING_STAFF);
 		maxStackSize = 1;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean flag)
+	{
+		if (!ItemNBTHelper.getString(itemStack, Names.NBTTags.STAFF_MOB_TO_SPAWN).isEmpty())
+		{
+			list.add("Mob to spawn: " + ItemNBTHelper.getString(itemStack, Names.NBTTags.STAFF_MOB_TO_SPAWN));
+		}
 	}
 }
--- src/main/java/com/themuso/mobsonyourterms/item/ItemStaffFragment.java
+++ src/main/java/com/themuso/mobsonyourterms/item/ItemStaffFragment.java
@@ -1,6 +1,15 @@
 package com.themuso.mobsonyourterms.item;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
 import com.themuso.mobsonyourterms.reference.Names;
+import com.themuso.mobsonyourterms.utility.ItemNBTHelper;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemStaffFragment extends ItemMOYT
 {
@@ -9,5 +18,14 @@ public class ItemStaffFragment extends ItemMOYT
 		super();
 		this.setUnlocalizedName(Names.Items.STAFF_FRAGMENT);
 		maxStackSize = 64;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean flag)
+	{
+		if (!ItemNBTHelper.getString(itemStack, Names.NBTTags.FRAGMENT_IS_FOR_MOB).isEmpty())
+		{
+			list.add("Mob to spawn: " + ItemNBTHelper.getString(itemStack, Names.NBTTags.FRAGMENT_IS_FOR_MOB));
+		}
 	}
 }
