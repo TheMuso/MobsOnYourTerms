eb6a76e4ed2623ac977ba2f2597181bebd018ff1
TheMuso <9444357+TheMuso@users.noreply.github.com>
2016-01-10 08:51:42 +1100

    Refine the PotentialSpawns event to make an exception for Extra Utilities cursed earth
    I must have been tired yesterday, and overlooked/missed something with this
    solution. What I originally came up with was unsatisfactory, hense the change
    back to what I had, with a slight refinement. Much less code, and more
    flexible.

--- src/main/java/com/themuso/mobsonyourterms/handler/RuleHandler.java
+++ src/main/java/com/themuso/mobsonyourterms/handler/RuleHandler.java
@@ -1,10 +1,12 @@
 package com.themuso.mobsonyourterms.handler;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
 
 import com.themuso.mobsonyourterms.reference.Settings;
+import com.themuso.mobsonyourterms.utility.LogHelper;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -16,6 +18,19 @@ public class RuleHandler
 		if ((!event.world.isRemote) && (!Settings.General.spawnHostileMobs) && (event.type == EnumCreatureType.monster))
 		{
 			event.setCanceled(true);
+
+			/* Exception for Extra Utilities cursed earth */
+			float x = event.x + 0.5F;
+			float y = event.y;
+			float z = event.z + 0.5F;
+
+			Block block = event.world.getBlock((int)x, (int)y, (int)z);
+			String blockName = block.getUnlocalizedName();
+
+			if (blockName.equals("tile.extrautils:cursedearthside"))
+			{
+				event.setCanceled(false);
+			}
 		}
 	}
 }
